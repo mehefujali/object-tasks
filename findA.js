3e6c4c711958a7bbfeed07e96292e4b34e9b8755
Mehefuj Ali <mehefujali30@gmail.com>
2024-09-05 11:20:05 +0530

    3 tasks

---
+++ findA.js
@@ -0,0 +1,14 @@
+let string = " hi i am kiron and i am a student"
+let countA = 0;
+
+for (let i = 0; i < string.length; i++) {
+    // console.log(string[i]);
+    if (string[i] === "a") {
+        countA++
+    }
+
+
+
+}
+
+console.log(countA);
